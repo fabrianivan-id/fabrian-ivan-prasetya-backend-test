1961eef9655e3484830e8cf897e393d8824ce61f
Fabrian Ivan Prasetya <fabrian.ivan@gmail.com>
2026-01-14 16:43:49 +0700

    commit

--- program/app.js
+++ program/app.js
@@ -5,21 +5,22 @@ const limit = 100;
 function solution(n) {
     let sum = n;
     while (n > 0){
-        sum += n % 0;
-        n = Math.floor(n/100);
+        sum += n % 10;
+        n = Math.floor(n/10);
     }
     return sum;
 }
 
+let total = 0;
 
-for (let i=1; i <= limit; i++){
+for (let i=1; i <= input; i++){
     const digits = i.toString().length;
-    const start = Math.max(1, x - (9 * digits));
+    const start = Math.max(1, i - (9 * digits));
 
     let hasGenerator = false;
 
     for (let g=start; g < i; g++){
-        if (g + solution(g) === x){
+        if (g + solution(g) === i){
             hasGenerator = true;
             break;
         }
